fb531b8b0244e04302d2402a4a4fc7b94ebadbd8
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-05-30 11:33:28 -0400

    Declaração de varivaies de escopo e de pacote

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import "fmt" //biblioteca padrão para E/S Formatada
+
+var variavel = "Variavel a nivel de pacote" // nivel de pacote
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	//Declaração de variavel - nivel de escopo
+	x := 10
+	y := "Olá bom dia"
+
+	fmt.Printf("varivael: %v, %T\n", variavel, variavel)
+
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("y: %v, %T\n\n", y, y)
+
+	x = 20 //oporador de atribuição
+	//x, z := 10, 5 //operador de declaração precisa ter pelo menos uma variavel nova
+
+	fmt.Printf("x: %v, %T\n\n", x, x)
+
 }
