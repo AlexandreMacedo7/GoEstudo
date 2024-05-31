7270ae46a33a68891770b06d8e303eff5c34ccf1
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-05-30 20:09:16 -0400

    Criando os proprios tipos

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
-import "fmt" //biblioteca padrão para E/S Formatada
+import (
+	"fmt"
+)
+
+// Criano os proprios tipos
+type hotdog int
+
+var b hotdog
 
 func main() {
 
-	x := "Oi "
-	y := "de novo"
-
-	//tipos de print
-
-	//print normal - função retornar algo na tela
-	fmt.Println("Olá")
-
-	//printS - retorna uma String para função
-	z := fmt.Sprint(x, y)
-
-	fmt.Println(z)
-
-	//Fprint writer interface
-
+	fmt.Printf("%T\n", b)
 }
