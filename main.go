2de3115bd564b04b2a64b9acedaaa069f2b57585
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-05-31 17:09:53 -0400

    Conversão de valores D - B- H

--- main.go
+++ main.go
@@ -4,21 +4,10 @@ import (
 	"fmt"
 )
 
-// Exercicio 5
-// Tipo tem como subjacente int, isso permite que um tipo int receba uma conversão de Tipo
-type tipo int
-
-var x tipo
-var y int
-
 func main() {
 
-	fmt.Printf("%v, %T\n", x, x)
-	x = 40
-	fmt.Printf("%v, %T\n", x, x)
+	a := 100
 
-	y = int(x)
-
-	fmt.Printf("%v, %T", y, y)
+	fmt.Printf("%d - %b - %#x", a, a, a)
 
 }
