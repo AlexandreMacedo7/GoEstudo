7bbf40a2b7045322eef7cb11b00af5a470e5473d
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-05-31 10:24:34 -0400

    Exercicio 5

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 )
 
-//Exercicio 4
-
+// Exercicio 5
+// Tipo tem como subjacente int, isso permite que um tipo int receba uma conversão de Tipo
 type tipo int
 
 var x tipo
+var y int
 
 func main() {
 
 	fmt.Printf("%v, %T\n", x, x)
 	x = 40
-	fmt.Printf("%v, %T", x, x)
+	fmt.Printf("%v, %T\n", x, x)
+
+	y = int(x)
+
+	fmt.Printf("%v, %T", y, y)
 
 }
