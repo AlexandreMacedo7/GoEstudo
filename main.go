c213eebee3025cf3252ff094204bd207c4685fea
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-06-01 18:37:42 -0400

    Nivel 2 - Exercicio 3

--- main.go
+++ main.go
@@ -5,15 +5,13 @@ import (
 )
 
 // Nivel 2
-// Exercicio 2
-func main() {
-	a := 10 == 20
-	b := 20 != 10
-	c := 30 >= 20
-	d := 40 < 20
-	e := 50 >= 30
-	f := 60 > 40
+// Exercicio 3
 
-	fmt.Printf("%v , %v, %v, %v, %v, %v", a, b, c, d, e, f)
+const a int = 10
+const b = 10
+
+func main() {
+
+	fmt.Printf("%v , %v", a, b)
 
 }
