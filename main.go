84c9f684d9ae5bb878ad215eab191bb5699124ac
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-06-01 18:32:08 -0400

    Nível 2 - Exercico 2

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 )
 
 // Nivel 2
-// Exercicio 1
+// Exercicio 2
 func main() {
-	a := 10
-	b := 20
-	c := 30
+	a := 10 == 20
+	b := 20 != 10
+	c := 30 >= 20
+	d := 40 < 20
+	e := 50 >= 30
+	f := 60 > 40
 
-	fmt.Printf("%d , %b, %#x", a, b, c)
+	fmt.Printf("%v , %v, %v, %v, %v, %v", a, b, c, d, e, f)
 
 }
