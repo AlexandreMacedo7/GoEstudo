23e824eb115f9102e9425f688c525fa80a98783e
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-06-02 12:17:08 -0400

    Nivel 3 - Exercicio 8

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 )
 
 // Nivel 3
-// Exercicio 7
+// Exercicio 8
 
 func main() {
 	a := 60
-	if a == 60 {
-		fmt.Print(true)
-	} else if a == 10 {
-		fmt.Print(false)
-	} else {
-		fmt.Print("Erro")
+	switch {
+	case a == 60:
+		fmt.Print("Igual a 60")
+	case a == 50:
+		fmt.Print("Não é igual a 60")
+	case a == 40:
+		fmt.Print("Não é igual a 60")
 	}
 
 }
